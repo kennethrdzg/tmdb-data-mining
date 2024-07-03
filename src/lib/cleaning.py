875309517293feff77687f2bc5f306ac516c07fc
Kenneth Rodriguez <kenneth.rodriguez.developer@gmail.com>
2024-07-02 18:56:42 -0600

    Agregar análisis de datos

--- src/lib/cleaning.py
+++ src/lib/cleaning.py
@@ -4,7 +4,6 @@ def clean_data(input_filename: str, output_filename: str)->pd.DataFrame:
     df = pd.read_csv(input_filename)
 
     print("Limpiando datos...")
-
     df = df.dropna()
     df = df.drop_duplicates()
     release_year = []
@@ -20,5 +19,6 @@ def clean_data(input_filename: str, output_filename: str)->pd.DataFrame:
     df['release_year'] = release_year
     df['release_month'] = release_month
     
+    print("Generando dataset/popular_movies.csv")
     df.to_csv(output_filename)
     print("Limpieza concluida.")
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 
 from lib.cleaning import clean_data
+from lib.analysis import analyze
 
 if __name__=="__main__": 
     dataset = "top_10000_popular_movies_tmdb.csv"
@@ -11,5 +12,9 @@ if __name__=="__main__":
     if not os.path.exists("../dataset/"+dataset): 
         print("No existe el dataset requerido")
         exit()
-    
-    clean_data(f'../dataset/{dataset}', f'../dataset/{final_dataset}')
+
+    print("Limpieza de datos")    
+    # clean_data(f'../dataset/{dataset}', f'../dataset/{final_dataset}')
+    print("-"*40)
+    print("Análisis de Datos:")
+    analyze(f'../dataset/{final_dataset}')
